6f4818ec84ce723b227aa5406929afaba46a4c44
SolitudeSF <solitudesf@protonmail.com>
2020-08-17 19:30:56 +0300

    Add wlp - setroot wrapper with multimonitor support. Version 0.1.5.

--- dotutils.nimble
+++ dotutils.nimble
@@ -1,13 +1,13 @@
 # Package
 
-version       = "0.1.4"
+version       = "0.1.5"
 author        = "SolitudeSF"
 description   = "Helper programs for my own workflow"
 license       = "MIT"
 srcDir        = "src"
-bin           = @["getprojecticon", "getmpdalbumart", "randfile"]
+bin           = @["getprojecticon", "getmpdalbumart", "randfile", "wlp"]
 
 
 # Dependencies
 
-requires "nim >= 0.20.0", "mpdclient >= 0.1.1"
+requires "nim >= 1.0.0", "mpdclient >= 0.1.5"
---
+++ src/wlp.nim
@@ -0,0 +1,46 @@
+import os, osproc, strutils
+
+proc setWallpapers(files: openArray[string]): int =
+  var args = @["-z"]
+  for n, path in files:
+    args.add "--on"
+    args.add $n
+    args.add path
+
+  let process = startProcess(findExe "setroot", args = args, options = {poParentStreams})
+
+  result = waitForExit process
+
+proc wlp =
+
+  let wallPath = getConfigDir() / "wlp" / "files"
+
+  var files: seq[string]
+
+  for line in wallPath.lines:
+    files.add line
+
+  let argc = paramCount()
+
+  if argc == 0:
+    quit setWallpapers files
+  else:
+    doAssert argc mod 2 == 0, "Provide xinerama monitor number and image path pairs."
+
+    let count = argc div 2
+
+    for i in 1..count:
+      let
+        n = paramStr(i * 2 - 1).parseInt
+        path = paramStr(i * 2)
+
+      files[n] = if path.isAbsolute: path else: getCurrentDir() / path
+
+    let file = open(wallPath, fmWrite)
+
+    for path in files:
+      file.writeLine path
+
+    quit setWallpapers files
+
+wlp()
